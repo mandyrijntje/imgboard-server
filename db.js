0e0193f5d8bba2097bc61882a99a281159eeaa25
messmonte <amandamariamonte@gmail.com>
2020-03-11 16:25:17 +0100

    imported and created db.js, synced database

---
+++ db.js
@@ -0,0 +1,13 @@
+const Sequelize = require("sequelize");
+const databaseURL =
+  process.env.DATABASE_URL ||
+  "postgres://postgres:secret@localhost:5432/postgres";
+const db = new Sequelize(databaseURL);
+
+db.sync({
+  force: false
+})
+  .then(console.log("Database synchronized"))
+  .catch(err => console.error(err));
+
+module.exports = db;
