bbcf483d813d2fd024f46cd64266e305a518680f
messmonte <amandamariamonte@gmail.com>
2020-03-11 18:12:23 +0100

    created post route in image router

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const express = require("express");
 const db = require("./db");
+const cors = require("cors");
+const bodyParser = require("body-parser");
 
 const Image = require("./image/model");
 const imageRouter = require("./image/router");
 
 const app = express();
+
+const corsMiddleware = cors();
+app.use(corsMiddleware);
+
+const parserMiddleware = bodyParser.json();
+app.use(parserMiddleware);
 
 const port = process.env.PORT || 4000;
 
