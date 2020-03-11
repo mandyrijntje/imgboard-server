878c5e629dbe58d36abf38ed467e518d78e5e257
messmonte <amandamariamonte@gmail.com>
2020-03-11 18:02:24 +0100

    created image router, modified index to use it

--- index.js
+++ index.js
@@ -2,9 +2,12 @@ const express = require("express");
 const db = require("./db");
 
 const Image = require("./image/model");
+const imageRouter = require("./image/router");
 
 const app = express();
 
 const port = process.env.PORT || 4000;
+
+app.use(imageRouter);
 
 app.listen(port, () => console.log(`App listening on port ${port}`));
