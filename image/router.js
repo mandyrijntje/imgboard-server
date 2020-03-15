4cf800c4517b5d6940e846f8b397c06413ded42e
messmonte <amandamariamonte@gmail.com>
2020-03-15 14:35:07 +0100

    added delete route to image/router

--- image/router.js
+++ image/router.js
@@ -1,5 +1,6 @@
 const { Router } = require("express");
 const Image = require("./model");
+const auth = require("../auth/middleware");
 
 const router = new Router();
 
@@ -15,15 +16,48 @@ async function getImages(req, res, next) {
 
 async function postImages(req, res, next) {
   try {
-    const createImage = await Image.create(req.body);
-
+    const fullResponse = { ...req.body, userId: req.user.dataValues.id };
+    const createImage = await Image.create(fullResponse);
     res.json(createImage);
   } catch (error) {
     next(error);
   }
 }
 
-router.get("/image", getImages);
-router.post("/image", postImages);
+async function deleteImage(req, res, next) {
+  try {
+    const imageToDelete = await Image.destroy({ where: { id: req.body.id } });
+    res.json(imageToDelete);
+  } catch (error) {
+    next(error);
+  }
+}
 
+async function getImagesByUser(req, res, next) {
+  try {
+    const imagesByUser = await Image.findAll({
+      where: { userId: req.params.id }
+    });
+    res.json(imagesByUser);
+  } catch (error) {
+    next(error);
+  }
+}
+
+async function updateImage(req, res, next) {
+  try {
+    const image = await Image.findByPk(req.params.id);
+    const updatedImage = await image.update(req.body);
+
+    res.json(updatedImage);
+  } catch (error) {
+    next(error);
+  }
+}
+
+router.get("/images", getImages);
+router.post("/images", auth, postImages);
+router.delete("/images", auth, deleteImage);
+router.get("/images/user/:id", getImagesByUser);
+router.put("/images/:id", updateImage);
 module.exports = router;
